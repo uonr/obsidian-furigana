0e09589c37d3ac849991f9cdb55a0f06e644aa0b
uonr <dev@yuru.me>
2025-02-04 00:24:15 +0900

    feat: context menu

--- main.ts
+++ main.ts
@@ -1,7 +1,40 @@
-import { Plugin, MarkdownView } from 'obsidian';
+import { Plugin, MarkdownView, Menu, Editor } from 'obsidian';
+
+const addRubyTag = (editor: Editor, selected: string) => {
+	const cursor = editor.getCursor();
+	const lineText = editor.getLine(cursor.line);
+	const rubyTagMatch = lineText.match(/<ruby\>(.*)<\/ruby>/);
+	let alreadyInRuby = (
+		rubyTagMatch &&
+		rubyTagMatch.index &&
+		rubyTagMatch.index < cursor.ch &&
+		rubyTagMatch.index + rubyTagMatch[0].length > cursor.ch
+	);
+	let head = `${selected}<rt>`;
+	let tail = "</rt>";
+	if (!alreadyInRuby) {
+		head = `<ruby>${head}`;
+		tail = `${tail}</ruby>`;
+	}
+	editor.replaceSelection(head + tail, "end");
+	const { line, ch } = editor.getCursor();
+	editor.setCursor({ line, ch: ch - tail.length });
+};
 
 export default class AliasPlugin extends Plugin {
 	async onload() {
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu: Menu, editor: Editor, view: MarkdownView) => {
+				const selectedText = editor.getSelection();
+				if (!selectedText || selectedText.trim() === "") {
+					return;
+				}
+				menu.addItem((item) => {
+					item.setTitle("Add <ruby> tag")
+						.onClick(() => addRubyTag(editor, selectedText));
+				});
+			})
+		);
 		this.addCommand({
 			id: 'add-ruby',
 			name: 'Add <ruby> tag for selected text',
@@ -10,28 +43,10 @@ export default class AliasPlugin extends Plugin {
 				if (!view) {
 					return false;
 				}
-				const editor = view.sourceMode.cmEditor;
+				const editor = view.editor;
 				if (!checking) {
 					const selected = editor.getSelection();
-					// check whether selected text already in a <ruby> tag.
-					const cursor = editor.getCursor();
-					const lineText = editor.getLine(cursor.line);
-					const rubyTagMatch = lineText.match(/<ruby\>(.*)<\/ruby>/);
-					let alreadyInRuby = (
-						rubyTagMatch &&
-						rubyTagMatch.index &&
-						rubyTagMatch.index < cursor.ch &&
-						rubyTagMatch.index + rubyTagMatch[0].length > cursor.ch
-					);
-					let head = `${selected}<rt>`;
-					let tail = "</rt>";
-					if (!alreadyInRuby) {
-						head = `<ruby>${head}`;
-						tail = `${tail}</ruby>`;
-					}
-					editor.replaceSelection(head + tail, "end");
-					const { line, ch } = editor.getCursor();
-					editor.setCursor({ line, ch: ch - tail.length });
+					addRubyTag(editor, selected);
 				}
 				return true;
 			}
